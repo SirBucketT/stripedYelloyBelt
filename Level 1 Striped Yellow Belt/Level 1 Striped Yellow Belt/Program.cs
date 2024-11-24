25ae1a6d9f2d0f393209c837fd67852b8810ac79
SirBucketT <tkpriva@icloud.com>
2024-11-24 20:26:42 +0100

    progress or something

--- Level 1 Striped Yellow Belt/Level 1 Striped Yellow Belt/Program.cs
+++ Level 1 Striped Yellow Belt/Level 1 Striped Yellow Belt/Program.cs
@@ -4,20 +4,52 @@ class Program
 {
     static void Main(string[] args)
     {
-        Random rnd = new Random();
-        int health = rnd.Next(0, 100);
         Random random = new Random();
-        int health2 = random.Next(0, 100);
+        Random random1 = new Random();
         
         Character Warrior = new Character
         {
             Name = "Warrior",
-            Health = health,
+            Health = random.Next(0, 100),
+            PrimaryAction = (targets) =>
+            {
+                Console.WriteLine("Warrior McWar!");
+                if (targets.Any())
+                {
+                    var target = targets.First();
+                    target.Health -= 20; 
+                    Console.WriteLine($"{target.Name} takes 20 damage! Health is now {target.Health}.");
+                }
+            }
         };
         Character Healer = new Character
         {
             Name = "Healer",
-            Health = health2,
-        };
+            Health = random1.Next(0, 100),
+            PrimaryAction = (targets) =>
+            {
+                if (targets.Any())
+                {
+                    var target = targets.OrderBy(t => t.Health).First();
+                    target.Health += 15; 
+                    Console.WriteLine($"{target.Name} is healed by 16! Health is now {target.Health}.");
+                }
+            }
+        };    
+        
+        List<Character> characters = new List<Character> { Warrior, Healer };
+        
+        Console.WriteLine($"{Healer.Name} HP: {Healer.Health}");
+        Console.WriteLine($"{Warrior.Name} HP: {Warrior.Health}");
+        
+        foreach (Character character in characters)
+        {
+            if (characters != null)
+            {
+                break;
+            }
+            
+            character.PrimaryAction(targets);
+        }
     }
 }
