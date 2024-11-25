e3f20763f2ab79c65c4df20682e87db29c3f04da
SirBucketT <tkpriva@icloud.com>
2024-11-25 14:44:34 +0100

    complete-ish kata

--- Level 1 Striped Yellow Belt/Level 1 Striped Yellow Belt/Program.cs
+++ Level 1 Striped Yellow Belt/Level 1 Striped Yellow Belt/Program.cs
@@ -2,57 +2,117 @@
 
 class Program
 {
-    static void Main()
+    static int Main()
     {
         Random random = new Random();
         Random random1 = new Random();
+        Random random2 = new Random();
+        Random random3 = new Random();
 
-        Character warrior = new Character("Warrior", random.Next(40, 51), primaryAction: (targets) =>
+        Character warrior = new Character("Warrior", random.Next(0, 101), primaryAction: (targets) =>
         {
-            Console.WriteLine("Warrior McWars!");
             if (targets.Any())
             {
-                var target = targets.First();
-                target.Health -= 20; 
-                Console.WriteLine($"{target.Name} takes 20 damage! Health is now {target.Health}.");
+                 var target = targets.First();
+                 target.Health -= 20;
+                 Console.WriteLine($"{target.Name} takes 20 damage! Health is now {target.Health}.");
             }
         });
 
-        Character healer = new Character("Healer", random1.Next(40, 51), primaryAction: (targets) =>
+        Character healer = new Character("Healer", random1.Next(0, 101), primaryAction: (targets) =>
         {
-            Console.WriteLine("Healer McHeals!");
-            if (targets.Any())
-            {
-                var target = targets.OrderBy(t => t.Health).First();
-                target.Health += 15; 
-                Console.WriteLine($"{target.Name} is healed by 16! Health is now {target.Health}.");
-            }
+             Console.WriteLine("Healer McHeals!");
+             if (targets.Any())
+             {
+                 var target = targets.OrderBy(t => t.Health).First();
+                 target.Health += 15;
+                 Console.WriteLine($"{target.Name} is healed by 16! Health is now {target.Health}.");
+             }
         });
-        
+        if (warrior.Health < 50 && healer.Health < 50)
+        {
+            warrior.Health = random2.Next(70, 100);
+            healer.Health = random3.Next(30, 50);
+        }
+        if (warrior.Health > 50 && healer.Health > 50)
+        {
+            warrior.Health = random2.Next(70, 100);
+            healer.Health = random3.Next(30, 50);
+        }
+
         List<Character> characters = new List<Character> { warrior, healer };
-        
+
         Console.WriteLine($"{healer.Name} HP: {healer.Health}");
         Console.WriteLine($"{warrior.Name} HP: {warrior.Health}");
         
-        foreach (Character character in characters)
+        bool charactersAreAlive = characters.Any();
+        while (charactersAreAlive)
         {
-            if (characters != null)
+            foreach (Character character in characters)
             {
-                break;
-            }
+                if (character.Health <= 0)
+                {
+                    Console.WriteLine($"{character.Name} is dead!");
+                    charactersAreAlive = false;
+                    //character.Health = 0;
+                    break;
+                }
+                if (character.Health < 50)
+                {
+                    warrior.PrimaryAction(characters);
+                    Console.WriteLine($"{warrior.Health} warrior health {healer.Health}!");
+                }
 
-            if (character.Health < 50)
-            {
-                Console.WriteLine($"{character.Name} prioritize attacking using McWars!");
-                target = target.Where(t => t.Name != "Healer").ToList();
-            }
+                if (character.Health >= 50)
+                {
+                    healer.PrimaryAction(characters);
+                }
 
-            if (character.Name == "Healer")
-            {
-                Console.WriteLine($"{character.Name} prioritize McHeals!");
+                Console.WriteLine($"Healer HP: {healer.Health}");
+                Console.WriteLine($"Warrior HP: {warrior.Health}");
             }
-            character.PrimaryAction(characters.First()); 
-            
         }
+
+        var warriors = characters.Where(t => t.Name != "Warrior").ToList();
+        var healers = warriors.Where(z => z.Name != "Healer").ToList();
+        return 0;
+        // while (warriors.Any(c => c.Health > 0))
+        // {
+        //     foreach (Character character in warriors)
+        //     {
+        //         if (character.Health <= 0)
+        //         {
+        //             Console.WriteLine($"{character.Name} is dead!");
+        //             break;
+        //         }
+        //         
+        //         if (character.Health < 50)
+        //         {
+        //             warrior = character;
+        //             Console.WriteLine($"{character.Name} prioritize attacking!");
+        //             character.Health -= 20;
+        //
+        //         }
+        //         
+        //         if (character.Name == "Healer")
+        //         {
+        //             healer = character;
+        //             Console.WriteLine($"{character.Name} prioritize McHeals!");
+        //             character.Health += 15;
+        //         }
+        //
+        //         character.PrimaryAction(warriors.First());
+        //
+        //         if (warriors.Count(c => c.Health > 0) <= 1)
+        //         {
+        //             Console.WriteLine("Loop condition met");
+        //             break;
+        //         }
+        //         foreach (var c in warriors)
+        //         {
+        //             Console.WriteLine($"{c.Name} HP: {c.Health}");
+        //         }
+        //     }
+        // }
     }
-}
+}    
