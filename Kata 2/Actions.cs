69163f7fdd68839ea12bf03f8f757040a86b2f32
SirBucketT <tkpriva@icloud.com>
2024-11-26 14:07:31 +0100

    kata 2 fixes

--- Kata 2/Actions.cs
+++ Kata 2/Actions.cs
@@ -4,42 +4,45 @@ public class Actions
 {
     public static void Game()
     {
-        Character arin = new Character("Arin", 90, characterAction: (targets) =>
+        Random rnd = new Random();
+        Random rnd2 = new Random();
+        int rdn1 = rnd.Next(0, 1000);
+        int rdn3 = rnd2.Next(0, 1000);
+        Character arin = new Character("Arin", rdn1, characterAction: (targets) =>
         {
             Console.WriteLine("Arin Attacks");
             if (targets.Any())
             {
                 var target = targets.OrderBy(t => t.Health).First();
                 target.Health -= 10;
-                Console.WriteLine($"Arin Attacks {target.Name}. {target.Name} Health {target.Health}");
+                Console.WriteLine($"Arin Attacks {target.Name}.");
+                target.HealthChanged += OnHealthChanged;
             }
         });
 
-        Character dalia = new Character("Dalia", 89, characterAction: (targets) =>
+        Character dalia = new Character("Dalia", rdn3, characterAction: (targets) =>
         {
             Console.WriteLine("Dalia Attacks");
             if (targets.Any())
             {
                 var target = targets.OrderBy(t => t.Health).First();
                 target.Health -= 10;
-                Console.WriteLine($"Dalia attacks {target.Name}. {target.Name} Health: {target.Health}");
+                Console.WriteLine($"Dalia attacks {target.Name}.");
                 target.HealthChanged += OnHealthChanged;
             }
         });
 
         static void OnHealthChanged(int health)
         {
-            Console.WriteLine($"Health changed to {health}");
+            Console.WriteLine($"health changed to {health}");
         }
 
         List<Character> characters = new List<Character> { arin, dalia };
         arin.CharacterAction(characters);
+        OnHealthChanged(arin.Health);
+        
         dalia.CharacterAction(characters);
-    }
-
-    public void OnHealthChanged(int health)
-    {
-        Console.WriteLine($"Health changed to {health}");
+        OnHealthChanged(dalia.Health);
     }
 }
     
