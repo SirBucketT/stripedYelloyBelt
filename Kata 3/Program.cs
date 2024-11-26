4f6e51bde580331260e2e15c7a03fe0db37407c2
Theodor Kvarnström <omega@hmm.local>
2024-11-26 18:49:33 +0100

    kata 3 changed

--- Kata 3/Program.cs
+++ Kata 3/Program.cs
@@ -4,8 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        var fireball = new AttackAbility("Fireball", "Deals 50 fire damage.");
-        var healingTouch = new HealAbility("Healing Touch", "Restores 30 health.");
+        var fireball = new AttackAbility("Hellfire", "Hellfire's inferno deals over 9000 damage, you got fried.");
+        var healingTouch = new HealAbility("Revival bead", "Revives deeply fried human.");
         
         var attackAbilityContainer = new AbilityContainer<AttackAbility>();
         var healAbilityContainer = new AbilityContainer<HealAbility>();
