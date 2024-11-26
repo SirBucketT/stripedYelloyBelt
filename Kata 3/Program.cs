b3ea0fdd2541e155a6e23c7bedba9849c481736c
SirBucketT <tkpriva@icloud.com>
2024-11-26 22:49:10 +0100

    kata completed

--- Kata 3/Program.cs
+++ Kata 3/Program.cs
@@ -2,24 +2,24 @@
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        var fireball = new AttackAbility("Hellfire", "Hellfire's inferno deals over 9000 damage, you got fried.");
-        var healingTouch = new HealAbility("Revival bead", "Revives deeply fried human.");
+        var hellfire = new AttackAbility("Hellfire", "Hellfire's inferno deals over 9000 damage, you got fried.");
+        var healing = new HealAbility("Revival bead", "Revives deeply fried human.");
         
         var attackAbilityContainer = new AbilityContainer<AttackAbility>();
         var healAbilityContainer = new AbilityContainer<HealAbility>();
         
-        attackAbilityContainer.AddAbility(fireball);
-        healAbilityContainer.AddAbility(healingTouch);
+        attackAbilityContainer.AddAbility(hellfire);
+        healAbilityContainer.AddAbility(healing);
         
         attackAbilityContainer.DisplayAbilities();
         healAbilityContainer.DisplayAbilities();
         
-        var retrieved = attackAbilityContainer.GetAbilityByName("Fireball");
+        var retrieved = attackAbilityContainer.GetAbilityByName("Hellfire");
         Console.WriteLine($"Retrieved Ability: {retrieved.Name} - {retrieved.Effect}");
         
-        attackAbilityContainer.RemoveAbility(fireball);
+        attackAbilityContainer.RemoveAbility(hellfire);
         attackAbilityContainer.DisplayAbilities();
     }
 }
